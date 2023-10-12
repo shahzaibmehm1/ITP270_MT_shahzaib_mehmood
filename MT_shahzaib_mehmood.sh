3aa57fdbd0db4961b555b32b37a59f589f1dfe7d
Cyber Range Student <student@kali.example.com>
2023-10-12 22:35:45 +0000

    adding git for the test

---
+++ MT_shahzaib_mehmood.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Prompt the user for input
+read  " (e.g., google.com): " 
+
+# Check if the input is "google.com"
+if [ "google.com" != "google.com" ]; then
+    echo "Input must be 'google.com'. 
+
+
+# Perform the ping and store results in a text file
+
+
+
+echo "Pinging $hostname and storing results in $ping_output_file..."
+ping -c 5 "$goggle.com" 
+
+
+
